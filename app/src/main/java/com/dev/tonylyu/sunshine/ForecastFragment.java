ac17da49f936da280983b8544017d5b9a1b74648
Tony <tony.lyu.dev@gmail.com>
2016-04-23 22:58:25 +0800

    add API key
    add API key in build.gradle

--- app/src/main/java/com/dev/tonylyu/sunshine/ForecastFragment.java
+++ app/src/main/java/com/dev/tonylyu/sunshine/ForecastFragment.java
@@ -117,7 +117,9 @@ public class ForecastFragment extends Fragment {
                 // Construct the URL for the OpenWeatherMap query
                 // Possible parameters are avaiable at OWM's forecast API page, at
                 // http://openweathermap.org/API#forecast
-                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+                String baseUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7";
+                String API_KEY = "&APPID" + BuildConfig.OPEN_WEATHER_MAP_API_KEY;
+                URL url = new URL(baseUrl.concat(API_KEY));
 
                 // Create the request to OpenWeatherMap, and open the connection
                 urlConnection = (HttpURLConnection) url.openConnection();
