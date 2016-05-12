6fac3dae61feff8e5f23047bc41a92f5e718bbcd
Tony Lyu <Tony.Lyu.dev@gmail.com>
2016-05-12 14:00:02 +0800

    test

--- app/src/main/java/com/dev/tonylyu/sunshine/DetailActivity.java
+++ app/src/main/java/com/dev/tonylyu/sunshine/DetailActivity.java
@@ -37,6 +37,7 @@ public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(LOG_TAG, "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         if (savedInstanceState == null) {
@@ -46,6 +47,35 @@ public class DetailActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onResume() {
+        Log.d(LOG_TAG, "onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(LOG_TAG, "onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(LOG_TAG, "onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(LOG_TAG, "onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(LOG_TAG, "onDestroy");
+        super.onDestroy();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
