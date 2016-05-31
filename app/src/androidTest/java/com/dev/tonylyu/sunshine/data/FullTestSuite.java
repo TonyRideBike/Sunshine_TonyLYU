504480a83205b20b6f15a0236c0d24e40bedf4d0
Tony Lyu <Tony.Lyu.dev@gmail.com>
2016-05-31 09:35:28 +0800

    db create

--- app/src/androidTest/java/com/dev/tonylyu/sunshine/data/FullTestSuite.java
+++ app/src/androidTest/java/com/dev/tonylyu/sunshine/data/FullTestSuite.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.dev.tonylyu.sunshine;
+package com.dev.tonylyu.sunshine.data;
 
 import android.test.suitebuilder.TestSuiteBuilder;
 
