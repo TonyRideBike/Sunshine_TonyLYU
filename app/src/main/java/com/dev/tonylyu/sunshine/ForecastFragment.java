2dcfdc5173e0b92c6f7400587d6c1c0889d1fc9a
Tony Lyu <Tony.Lyu.dev@gmail.com>
2017-03-06 16:04:04 +0800

    back on track

--- app/src/main/java/com/dev/tonylyu/sunshine/ForecastFragment.java
+++ app/src/main/java/com/dev/tonylyu/sunshine/ForecastFragment.java
@@ -153,7 +153,7 @@ public class ForecastFragment extends Fragment {
                         .appendQueryParameter(FORMAT_PARAM, format)
                         .appendQueryParameter(UNITS_PARAM, units)
                         .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
-                        .appendQueryParameter(APPID_PARAM, BuildConfig.OPEN_WEATHER_MAP_API_KEY)
+                        .appendQueryParameter(APPID_PARAM, BuildConfig.API_KEY)
                         .build();
                 URL url = new URL(uri.toString());
 
--- app/src/main/java/com/dev/tonylyu/sunshine/data/WeatherContract.java
+++ app/src/main/java/com/dev/tonylyu/sunshine/data/WeatherContract.java
@@ -62,7 +62,6 @@ public class WeatherContract {
 
         public static final String CONTENT_TYPE =
                 ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
-
         public static final String CONTENT_ITEM_TYPE =
                 ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
 
@@ -71,11 +70,9 @@ public class WeatherContract {
 
         // The location setting string is what will be sent to openweathermap as the location query
         public static final String COLUMN_LOCATION_SETTING = "location_setting";
-
         // Human readable location string, provided by the API.
         // Because for styling, "Mountain View" is more recognizable than 94043.
         public static final String COLUMN_CITY_NAME = "city_name";
-
         // In order to uniquely pinpoint the location on the map when we lauch the map intent,
         // we store the latitude and longitude as returned by openweathermap.
         public static final String COLUMN_COORD_LAT = "coord_lat";
@@ -94,7 +91,6 @@ public class WeatherContract {
 
         public static final String CONTENT_TYPE =
                 ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
-
         public static final String CONTENT_ITEM_TYPE =
                 ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
 
