b2bde97bf6a48c3cc55552a51516f88783a6d925
Tony Lyu <tony.lyu.dev@gmail.com>
2017-03-08 20:25:10 +0800

    Start Using CursorAdapter

--- app/src/androidTest/java/com/dev/tonylyu/sunshine/TestFetchWeatherTask.java
+++ app/src/androidTest/java/com/dev/tonylyu/sunshine/TestFetchWeatherTask.java
@@ -22,10 +22,10 @@ import android.test.AndroidTestCase;
 import com.dev.tonylyu.sunshine.data.WeatherContract;
 
 public class TestFetchWeatherTask extends AndroidTestCase {
-    static final String ADD_LOCATION_SETTING = "Sunnydale, CA";
-    static final String ADD_LOCATION_CITY = "Sunnydale";
-    static final double ADD_LOCATION_LAT = 34.425833;
-    static final double ADD_LOCATION_LON = -119.714167;
+    private static final String ADD_LOCATION_SETTING = "Sunnydale, CA";
+    private static final String ADD_LOCATION_CITY = "Sunnydale";
+    private static final double ADD_LOCATION_LAT = 34.425833;
+    private static final double ADD_LOCATION_LON = -119.714167;
 
     // TODO: 16/6/10 uncomment testAddLocation after you have written the AddLocation function.
     // This test will only run on API level 11 and higher because of a requirement in the
