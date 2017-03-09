ef8b86ac8ddc6bd98b744396c132a5476ab6c0cc
Tony Lyu <tony.lyu.dev@gmail.com>
2017-03-09 22:01:21 +0800

    Using CursorAdapter

--- app/src/main/java/com/dev/tonylyu/sunshine/ForecastFragment.java
+++ app/src/main/java/com/dev/tonylyu/sunshine/ForecastFragment.java
@@ -26,7 +26,7 @@ public class ForecastFragment extends Fragment implements
 
     private static final int WEATHER_LOADER_ID = 101;
     private final String LOG_TAG = ForecastFragment.class.getSimpleName();
-    ForecastAdapter mForecastAdapter;
+    private ForecastAdapter mForecastAdapter;
 
     public ForecastFragment() {
         // Required empty public constructor
@@ -65,44 +65,13 @@ public class ForecastFragment extends Fragment implements
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        // Now that we have some dummy forecast data, create an ArrayAdapter.
-        // The ArrayAdapter will take data from a source (like our dummy forecast) and
-        // use it to populate the ListView it's attached to.
-//        mForecastAdapter =
-//                new ArrayAdapter<>(
-//                        getActivity(), // The current context (this activity)
-//                        R.layout.list_item_forecast, // The name of the layout ID.
-//                        R.id.list_item_forecast_textview, // The ID of the textview to populate.
-//                        new ArrayList<String>());
-
-//        String location_setting = Utility.getPreferredLocation(getContext());
-//
-//        String sort_order = WeatherContract.WeatherEntry.COLUMN_DATE + " ASC";
-//        Uri weatherLocationUri = WeatherContract.WeatherEntry.buildWeatherLocationWithStartDate(
-//                location_setting, System.currentTimeMillis());
-//        Cursor cursor = getContext().getContentResolver().query(
-//                weatherLocationUri,
-//                null, null, null, sort_order
-//        );
-//
-//        mForecastAdapter = new ForecastAdapter(getContext(), cursor, 0);
-
-        mForecastAdapter = new ForecastAdapter(getContext(), null, 0);
+        mForecastAdapter = new ForecastAdapter(getActivity(), null, 0);
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
         // Get a reference to the ListView, and attach this adapter to it.
         ListView listView = (ListView) rootView.findViewById(R.id.list_view_forecast);
         listView.setAdapter(mForecastAdapter);
-//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                String forecast = mForecastAdapter.getItem(position);
-//                Intent intent = new Intent(getActivity(), DetailActivity.class);
-//                intent.putExtra(Intent.EXTRA_TEXT, forecast);
-//                startActivity(intent);
-//            }
-//        });
 
         return rootView;
     }
@@ -113,8 +82,8 @@ public class ForecastFragment extends Fragment implements
             Log.d(LOG_TAG, "updateWeather Start.");
         }
 
-        FetchWeatherTask weatherTask = new FetchWeatherTask(getContext());
-        String location = Utility.getPreferredLocation(getContext());
+        FetchWeatherTask weatherTask = new FetchWeatherTask(getActivity());
+        String location = Utility.getPreferredLocation(getActivity());
 
         weatherTask.execute(location);
     }
